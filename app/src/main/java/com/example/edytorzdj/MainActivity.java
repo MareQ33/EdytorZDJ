48be97eaf2ef135191444ec37c53125381a8488d
valor <valor@LAPTOP-3CN1CA58>
2022-10-24 00:09:51 +0200

    Zrobiona część Layoutu

--- app/src/main/java/com/example/edytorzdj/MainActivity.java
+++ app/src/main/java/com/example/edytorzdj/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.edytorzdj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.Veigar);
+        SeekBar seekBar1 = findViewById(R.id.seek1);
+        SeekBar seekBar2 = findViewById(R.id.seek2);
+        SeekBar seekBar3 = findViewById(R.id.seek3);
+
+
     }
 }
